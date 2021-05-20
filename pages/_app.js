387caf11a769563d38ae6a1f28b014ee95a16a70
Radosław Biesek <radoslaw.biesek@profil-software.com>
2021-05-20 19:26:16 +0200

    Add app comments

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import Layout from "../components/layout/Layout";
 
 import '../styles/global.css';
 
+// persisting layout between page changes
+// keeping state when navigating
+// add global css
+
 const app = ({ Component, pageProps }) => (
   <Layout>
     <Component {...pageProps} />
